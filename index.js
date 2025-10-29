5024e5c42e226ffab529a8d6a74e733cecfd6cd5
AbdulhaqqIbrahim <abdulhaqq.ibrahim27@compscihigh.org>
2025-10-29 14:15:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,29 +8,38 @@
   "The value stored in the variable [variableName] is [value] and its data type is [type]."
 */
 
+
+
 // 1️⃣ String
 // Declare a variable named studentName and store your name as a string.
 // Use string interpolation to log the sentence describing its value and type.
-
+let studentName = "Abdulhaqq Ibrahim"
+console.log(`My name is ${studenName} and the data type is a string`)
 // 2️⃣ Number
 // Declare a variable named studentAge and store your age as a number.
 // Use string interpolation to log the sentence describing its value and type.
-
+let studentAge = 17
+console.log(`My age is ${studenAame} and the data type is a number`)
 // 3️⃣ Boolean
 // Declare a variable named isEnrolled and assign it either true or false.
 // Use string interpolation to log the sentence describing its value and type.
-
+let isEnrolled = true
+console.log(`I am enrolled  ${isEnrolled} and the data type is a boolean`)
 // 4️⃣ Undefined
 // Declare a variable named favoriteSubject but do not assign it a value.
 // Use string interpolation to log the sentence describing its value and type.
+let favoriteSubject
 
+console.log(`My favorite subject is ${favoriteSubject} and the data type is unndefined
 // 5️⃣ Null
 // Declare a variable named nextClass and set it equal to null.
 // Use string interpolation to log the sentence describing its value and type.
-
+let nextClass = null
+console.log(`My next class is ${nextClass} and the data type is null`) 
 // 6️⃣ Check
 // When finished, run your file to verify that all outputs print correctly.
 // Run your file by typing node index.js in the terminal
 
 // 6️⃣ Commit & Push
 // Save your work and push it for grading
+
